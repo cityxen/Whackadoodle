a7e2c6bc144d7459e03a4fb763ece49cb1dad2d6
deadline-cxn <deadline@cityxen.net>
2024-07-16 23:58:45 -0400

    exomized basic thing

--- Build.bat
+++ Build.bat
@@ -7,7 +7,8 @@ echo Build Script: Building %1
 call genkickass-script.bat -t C64 -o prg_files -m true -s true -l "RETRO_DEV_LIB"
 call KickAss.bat whackadoodle.asm
 
-// exomizer raw systrim -o prg_files\\wad-cxn-e.prg prg_files\\wad-cxn-w.prg w
+xcopy prg_files\\wad-cxn.prg prg_files\\wad-cxn-w.prg /Y
+exomizer sfx basic -o prg_files\\wad-cxn-e.prg prg_files\\wad-cxn-w.prg 
 xcopy prg_files\\wad-cxn-e.prg prg_files\\wad-cxn.prg /Y
 xcopy prg_files\\wad-cxn-e.prg x:\\www\\tech.cityxen.net\\html\\m64\\games\\wad-cxn.prg /Y
 
